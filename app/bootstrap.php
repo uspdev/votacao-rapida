979f954e96a4c237200e842945cc8d5c292606bb
Masaki Kawabata Neto <masaki.kawabata@gmail.com>
2020-04-06 20:08:44 -0300

    usando phpdotenv

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,22 +4,16 @@ require_once __DIR__ . '/functions.php';
 
 use \RedBeanPHP\R as R;
 
-# VOTACAO
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
+# Arquivos locais
 putenv('USPDEV_VOTACAO_LOCAL=' . realpath(__DIR__ . '/../local'));
 
 // caminho da API
-putenv('USPDEV_VOTACAO_API=http://localhost/git/uspdev/votacao/public/api');
+putenv('USPDEV_VOTACAO_API=' . getenv('WWWROOT') . '/api');
 
-//R::addDatabase('votacao', 'sqlite:' . getenv('USPDEV_VOTACAO_LOCAL') . '/votacao.db3');
-R::addDatabase('votacao', 'mysql:host=localhost;dbname=votacao', 'votacao', 'votacao');
-
-R::selectDatabase('votacao');
-R::useFeatureSet('latest');
-R::freeze(false);
-
-// --------------------------------
-$amb = 'dev';
+$amb = getenv('AMBIENTE');
 if ($amb == 'dev') {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
@@ -27,15 +21,15 @@ if ($amb == 'dev') {
     # 1-faz o navegador solicitar as credenciais do usuário;
     # 0-nega acesso (default)
     putenv('USPDEV_WEBSERVICE_USER_FRIENDLY=0');
+
+    # veja https://github.com/uspdev/cache
+    putenv('USPDEV_CACHE_DISABLE=1');
 }
 
 if ($amb == 'prod') {
     ini_set('display_errors', 0);
     ini_set('display_startup_errors', 0);
 }
-
-# veja https://github.com/uspdev/cache
-putenv('USPDEV_CACHE_DISABLE=1');
 
 # veja https://github.com/uspdev/ip-control
 putenv('USPDEV_IP_CONTROL=localhost');
@@ -45,15 +39,27 @@ $base = Flight::request()->base;
 $base = (substr($base, -1) == '/') ? substr($base, 0, -1) : $base;
 
 # O dominio vamos tentar adivinhar, se não for em linha de comando
-if (PHP_SAPI != 'cli') {
-    putenv('DOMINIO=http://' . $_SERVER['HTTP_HOST'] . $base);
-}
+// if (PHP_SAPI != 'cli') {
+//     putenv('DOMINIO=http://' . $_SERVER['HTTP_HOST'] . $base);
+// }
 
 # Local onde o webservice colocará arquivos sqlite, logs, etc.
 # Mandatário
 putenv('USPDEV_WEBSERVICE_LOCAL=' . getenv('USPDEV_VOTACAO_LOCAL'));
 
-# Rota para gerencimaneto do webservice . default='ws'
+# Rota para gerenciamento do webservice. default='ws'
 putenv('USPDEV_WEBSERVICE_ADMIN_ROUTE=ws');
+
+// Conexão com DB
+if (getenv('DB_TIPO') == 'sqlite') {
+    R::addDatabase('votacao', 'sqlite:' . getenv('USPDEV_VOTACAO_LOCAL') . '/votacao.db3');
+} else {
+    R::addDatabase('votacao', getenv('DB_DSN'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'));
+}
+
+R::selectDatabase('votacao');
+R::useFeatureSet('latest');
+R::freeze(false);
+
 
 require_once __DIR__ . '/config.php';
