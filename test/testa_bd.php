61394a2a1ab084862590763e9b325f74fd4dc7ec
Masaki Kawabata Neto <masaki.kawabata@gmail.com>
2020-04-12 18:01:35 -0300

    msg

--- test/testa_bd.php
+++ test/testa_bd.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../app/bootstrap.php';
 
 use \RedBeanPHP\R as R;
-
+echo "Conectando em ", getenv('DB_DSN'), PHP_EOL;
 R::setup(getenv('DB_DSN'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'));
 $listOfTables = R::inspect();
 print_r($listOfTables);
