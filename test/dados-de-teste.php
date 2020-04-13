1d1c720311b058467980564c82e98205b4b9204c
Masaki Kawabata Neto <masaki.kawabata@gmail.com>
2020-04-13 20:27:49 -0300

    melhorando o demo

--- test/dados-de-teste.php
+++ test/dados-de-teste.php
@@ -9,7 +9,7 @@ $e = 'e.usp.br/fiv';
 $lista = 'João, Adriana, Maria, Carlos, Antônio, Marcela, Edson';
 
 //gerando 10 tokens
-$tokens = gerarTokens(10, true);
+$tokens = gerarTokens(20, true);
 
 // se não quiser logo secundário, deixar em branco
 $logo2 = '';
@@ -29,37 +29,37 @@ $sessao = [
         [
             '_type' => 'votacao', // fixo
             'estado' => 0, // sempre inicia fechado
-            'nome' => 'Você gostou da votação eletrônica?',
+            'nome' => 'Qual a sua primeira impressão ao utilizar a votação rápida?',
             'descricao' => 'Assinale uma alternativa',
             'tipo' => 'aberta',
             'input_type' => 'checkbox',
-            'input_count' => '3',
+            'input_count' => '1',
             'data_ini' => '',
             'data_fim' => '',
             'ownAlternativaList' => [
                 [
                     '_type' => 'alternativa',
-                    'texto' => 'Armando Sales',
+                    'texto' => 'Não gostei',
                 ],
                 [
                     '_type' => 'alternativa',
-                    'texto' => 'Oswaldo Cruz',
+                    'texto' => 'Achei legalzinho',
                 ],
                 [
                     '_type' => 'alternativa',
-                    'texto' => 'Dom Pedro',
+                    'texto' => 'Achei excelente',
                 ],
                 [
                     '_type' => 'alternativa',
-                    'texto' => 'Marie Curie',
+                    'texto' => 'Vai resolver todos os problemas do mundo!',
                 ],
             ],
         ],
         [
             '_type' => 'votacao',
-            'estado' => 0, // fechado
-            'nome' => 'Você é a favor de usar votação eletrônica?',
-            'descricao' => '',
+            'estado' => 0,
+            'nome' => 'Qual a sua segunda impressão ao utilizar a votação rápida?',
+            'descricao' => 'Agora que você já viu o processo completo da votação, assinale a opção que reflete seu grau de satisfação.',
             'tipo' => 'fechada',
             'input_type' => 'radio',
             'input_count' => '1',
@@ -68,11 +68,15 @@ $sessao = [
             'ownAlternativaList' => [
                 [
                     '_type' => 'alternativa',
-                    'texto' => 'Sim',
+                    'texto' => 'Realmente não gostei, precisa melhorar.',
                 ],
                 [
                     '_type' => 'alternativa',
-                    'texto' => 'Não',
+                    'texto' => 'Agora que ví o funcionamento, acho que vai ajudar bastante.',
+                ],
+                [
+                    '_type' => 'alternativa',
+                    'texto' => 'Tenho certeza que a votação rápida vai trazer a paz mundial!',
                 ],
             ],
         ],
