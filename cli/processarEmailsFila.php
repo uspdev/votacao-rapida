c9b6a12b3123be4d0940d908a4a1e6064754c13a
Masaki Kawabata Neto <masaki.kawabata@gmail.com>
2020-04-29 14:37:18 -0300

    documentação interna

--- cli/processarEmailsFila.php
+++ cli/processarEmailsFila.php
@@ -3,7 +3,9 @@ require_once __DIR__ . '/../app/bootstrap.php';
 require_once __DIR__ . '/funcoes_cli.php';
 
 use Uspdev\Votacao\Email;
-
+/**
+ * Este script é chamado pelo sistema para enviar emails que estão em fila
+ * A fila é na tabela email. Ao enviar, ele registra a data e hora do envio.
+ */
 $count = Email::processarFila();
-
 echo 'Enviados ' . $count . ' emails.'.PHP_EOL;
