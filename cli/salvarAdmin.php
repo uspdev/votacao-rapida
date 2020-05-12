7a0958c9aa1849e37dc6aaecc177c63e5fa514ee
Masaki Kawabata Neto <masaki.kawabata@gmail.com>
2020-05-12 16:03:19 -0300

    minor

--- cli/salvarAdmin.php
+++ cli/salvarAdmin.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../app/bootstrap.php';
 require_once __DIR__ . '/funcoes_cli.php';
 
 if (!empty($argv[1])) {
-    echo salvarAdmin(1575309), PHP_EOL;
+    echo salvarAdmin($argv[1]), PHP_EOL;
 } else {
     echo 'Uso: php salvar_admin.php <codpes>', PHP_EOL;
 }
