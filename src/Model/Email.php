3612693b7a502ed0670920e7c12730cded3b2fdc
Masaki Kawabata Neto <masaki.kawabata@gmail.com>
2020-08-03 20:43:10 -0300

    oportunistic tsl

--- src/Model/Email.php
+++ src/Model/Email.php
@@ -243,7 +243,7 @@ class Email
 
         $mail->SMTPKeepAlive = true;
         $mail->Host = "smtp.gmail.com";
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        //$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->SMTPAuth = true;
         $mail->Username = getenv('EMAIL');
         $mail->Password = getenv('EMAIL_PWD');
