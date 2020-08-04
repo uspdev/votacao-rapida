5a2c9c29e81a214c363f11312fbc74b796a384dc
Masaki Kawabata Neto <masaki.kawabata@gmail.com>
2020-08-04 15:46:45 -0300

    atualizando

--- app/get_oauth_token.php
+++ app/get_oauth_token.php
@@ -74,8 +74,8 @@ if (!in_array($providerName, ['Google', 'Microsoft', 'Yahoo'])) {
 
 //These details are obtained by setting up an app in the Google developer console,
 //or whichever provider you're using.
-$clientId = '835710592525-h667t5cvqkv7bvfrbpuchbpqgum3qdnv.apps.googleusercontent.com';
-$clientSecret = 'j2y7C4MDthAKhQdmCPyquYqp';
+$clientId = getenv('EMAIL_OAUTH_CLIENT_ID');
+$clientSecret = getenv('EMAIL_OAUTH_CLIENT_SECRET');
 
 //If this automatic URL doesn't work, set it yourself manually to the URL of this script
 //$redirectUri = (isset($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
